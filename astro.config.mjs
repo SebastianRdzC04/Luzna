336994e6d73a46d8087c7ed420195e980a8a8109
SebastianRdzC04 <22030138@uttcampus.edu.mx>
2025-07-22 00:45:59 -0600

    el loader ya debe estar funcionando a ver que rollo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import lottie from 'astro-integration-lottie';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), lottie()]
 });
